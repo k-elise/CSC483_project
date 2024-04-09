d47cc3c62919a16656fe10413c1538f388e45838
NoelleHS <nhealeystewart@arizona.edu>
2024-04-08 19:05:08 -0700

    adding maven dependencies

--- proj/src/index.java
+++ proj/src/index.java
@@ -1,10 +1,49 @@
 
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.ByteBuffersDirectory;
 
 public class index {
 
+	StandardAnalyzer analyzer = new StandardAnalyzer();
+	Directory index = new ByteBuffersDirectory();
+	IndexWriterConfig config = new IndexWriterConfig(analyzer);
+	
+	private void buildIndex() {
+        //Get file from resources folder
+        
+    }
+	
 	public static void main(String[] args) {
 		System.out.println("Hello.");
 	}
+	
+	
+	
+	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+		  Document doc = new Document();
+		  doc.add(new TextField("title", title, Field.Store.YES));
+		  doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		  w.addDocument(doc);
+		}
 }
 
